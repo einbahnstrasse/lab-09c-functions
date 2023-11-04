a9834f628acb05d08fef54f231d4a8c6dd8d8904
Louis Goldford <louisgoldford@LG-2021-iMac.fritz.box>
2023-11-04 10:50:13 +0100

    +week 9 stuff

---
+++ double.py
@@ -0,0 +1,7 @@
+def double(n):
+    return n * 2
+
+userstring = input("Number please: ")
+usernum = int(userstring)
+
+print(double(usernum))
---
+++ pluralize.py
@@ -0,0 +1,5 @@
+def pluralize(word):
+    return word + "s"
+
+userWord = input("Word please: ")
+print(pluralize(userWord))
